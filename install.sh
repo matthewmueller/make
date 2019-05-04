fd4aab9ddf1e469aeb13ca442417350883ace975
Matthew Mueller <mattmuelle@gmail.com>
2019-05-04 17:50:39 +0700

    update install script

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 mkdir -p /usr/local/include/github.com/matthewmueller/make
 curl -sL https://github.com/matthewmueller/make/archive/master.tar.gz \
-  | tar xz --wildcards "*.mk" --strip-components=1 -C /usr/local/include/github.com/matthewmueller/make
+  | tar xz --strip-components=1  --include "*/*.mk" --include "*/config/" -C /usr/local/include/github.com/matthewmueller/make
