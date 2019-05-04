f8ba099a268ee6518a64bcdd4a2be9345aa36e1e
Matthew Mueller <mattmuelle@gmail.com>
2019-05-04 18:17:33 +0700

    update install script

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ INCLUDE_DIR="/usr/local/include/github.com/matthewmueller/make"
 TMP_DIR=$(mktemp -d)
 
 curl -sL https://github.com/matthewmueller/make/archive/master.tar.gz \
-  | tar xz --strip-components=1  --include "*/*.mk" --include "*/config/" -C "${TMP_DIR}"
+  | tar xz --strip-components=1  --exclude "*/test" -C "${TMP_DIR}"
 
 # clear old version
 rm -rf "${INCLUDE_DIR}"
