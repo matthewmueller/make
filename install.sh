157b89e0253a22edc279a9f3da722e038c93612a
Matthew Mueller <mattmuelle@gmail.com>
2019-05-04 18:29:48 +0700

    fix install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-INCLUDE_DIR="/usr/local/include/github.com/matthewmueller/make"
+INCLUDE_DIR="/usr/local/include/github.com/matthewmueller/make/"
 TMP_DIR=$(mktemp -d)
 
 curl -sL https://github.com/matthewmueller/make/archive/master.tar.gz \
