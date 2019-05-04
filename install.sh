10fbc20afe3e5bbff116223f8c305169809b2688
Matthew Mueller <mattmuelle@gmail.com>
2019-05-04 18:36:11 +0700

    fix install script

--- install.sh
+++ install.sh
@@ -9,4 +9,4 @@ curl -sL https://github.com/matthewmueller/make/archive/master.tar.gz \
 # clear old version
 rm -rf "${INCLUDE_DIR}"
 mkdir -p "${INCLUDE_DIR}"
-mv "${TMP_DIR}/*" "${INCLUDE_DIR}"
+mv "${TMP_DIR}"/* "${INCLUDE_DIR}"
