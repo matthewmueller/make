73aea2537b5b6acc22d8a65431907146d99bba59
Matthew Mueller <mattmuelle@gmail.com>
2019-05-04 17:55:44 +0700

    fix read

--- read.mk
+++ read.mk
@@ -1,10 +1,13 @@
 # Always prompt the user, but if the variable is in scope,
 # use that value as the default. Use $$(cat $(TEMP)/___)
 # to read the result, where ___ is the name of the variable
-.PHONY: read read. read.%
-read.%: TEMP := $(or ${TMPDIR},${TMP},${TEMP},/tmp)/make
-read.%: $(TEMP)
-	@ mkdir -p $(TEMP)
+read.%: TEMP := $(or ${TMPDIR},${TMP},${TEMP})
+read.%:
+	@ if [ "${TEMP}" = "" ]; then \
+		echo "Required temporary directory not found"; \
+		exit 1; \
+	fi
+	@ mkdir -p ${TEMP}/make
 	@ if [ "${${*}}" = "" ]; then \
 			while [ "$${input}" = "" ]; do \
 				echo "Enter a value for ${*}: \c"; \
@@ -14,18 +17,23 @@ read.%: $(TEMP)
 			echo "Enter a value for ${*} (default: ${${*}}): \c"; \
 			read input; \
 		fi; \
-		echo "$${input:-${${*}}}" > ${TEMP}/${*}
+		echo "$${input:-${${*}}}" > ${TEMP}/make/${*}
+.PHONY: read read. read.%
 
 # First try reading from an environment variable.
 # If the env is empty, prompt the user for a value
 # Use $$(cat $(TEMP)/___) to read the result, where
 # ___ is the name of the variable
-.PHONY: eread eread. eread.%
-eread.%: TEMP := $(or ${TMPDIR},${TMP},${TEMP},/tmp)/make
-eread.%: $(TEMP)
-	@ mkdir -p $(TEMP)
+eread.%: TEMP := $(or ${TMPDIR},${TMP},${TEMP})
+eread.%:
+	@ if [ "${TEMP}" = "" ]; then \
+		echo "Required temporary directory not found"; \
+		exit 1; \
+	fi
+	@ mkdir -p ${TEMP}/make
 	@ while [ "$${input:-${${*}}}" = "" ]; do \
 			echo "Enter a value for ${*}: \c"; \
 			read input; \
 		done; \
-		echo "$${input:-${${*}}}" > ${TEMP}/${*}
+		echo "$${input:-${${*}}}" > ${TEMP}/make/${*}
+.PHONY: eread eread. eread.%
