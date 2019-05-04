d365d2d705595d9140ef83788bbfc3498cd62a3a
Matthew Mueller <mattmuelle@gmail.com>
2019-05-04 18:18:49 +0700

    a few more excluded files

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ INCLUDE_DIR="/usr/local/include/github.com/matthewmueller/make"
 TMP_DIR=$(mktemp -d)
 
 curl -sL https://github.com/matthewmueller/make/archive/master.tar.gz \
-  | tar xz --strip-components=1  --exclude "*/test" -C "${TMP_DIR}"
+  | tar xz --strip-components=1  --exclude "*/test" --exclude "*/install.sh"  --exclude "*/Readme.md" -C "${TMP_DIR}"
 
 # clear old version
 rm -rf "${INCLUDE_DIR}"
