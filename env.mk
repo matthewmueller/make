408dac5f2e2fd7faeefbeaff7f7a7f74db1eac01
Matthew Mueller <mattmuelle@gmail.com>
2019-06-09 22:35:45 +0700

    fix env for environment variables with spaces in them

--- env.mk
+++ env.mk
@@ -1,20 +1,7 @@
 ## Ensure we have the provided environment variable
 env.%:
-	@ if [ "${${*}}" = "" ]; then \
+	@ if [ ${${*}} = "" ]; then \
 		echo "Required variable \`${*}\` is not set."; \
 		exit 1; \
 	fi
 .PHONY: env env. env.%
-
-## Ensure we have the provided environment variable
-## ucenv ensures that the input is all uppercase
-ucenv.%:
-	@ if [ "$$(command -v tr)" = "" ]; then \
-		echo "\n  Error: Required dependency \`tr\` is not found in your PATH.\n"; \
-		exit 1; \
-	fi
-	@ if [ "${$$(echo '${*}' | tr a-z A-Z)}" = "" ]; then \
-		echo "\n  Error: You must pass in the \`$$(echo '${*}' | tr a-z A-Z)\` environment variable.\n"; \
-		exit 1; \
-	fi
-.PHONY: uenv uenv. uenv.%
